6d0e5014f6fdb21bd6fe335c0015bc498a42b976
Elijah <flemingelijah93@gmail.com>
2026-04-04 14:17:17 -0600

    Add CDK ECR stack for demo inference API

--- infra/cdk/bin/cdk.ts
+++ infra/cdk/bin/cdk.ts
@@ -4,9 +4,9 @@ import { EcrStack } from '../lib/ecr-stack';
 
 const app = new cdk.App();
 
-new EcrStack(app, 'LlmParityDevStack', {
+new EcrStack(app, 'LlmParityDevEcrStack', {
   env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
+    account: process.env.CDK_DEFAULT_ACCOUNT ?? '655469962246',
     region: 'us-east-2',
   },
 });
