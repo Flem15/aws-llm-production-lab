cdf7cf8e3791cb117e46ecdece96a2f1b595abe9
Elijah <flemingelijah93@gmail.com>
2026-04-04 13:02:54 -0600

    Creates a dedicated CDK stack for ECR and defines one repository: demo-inference-api

---
+++ infra/cdk/lib/ecr-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+
+export class EcrStack extends cdk.Stack {
+  public readonly repository: ecr.Repository;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.repository = new ecr.Repository(this, 'DemoInferenceRepository', {
+      repositoryName: 'demo-inference-api',
+      imageScanOnPush: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      emptyOnDelete: true,
+    });
+
+    new cdk.CfnOutput(this, 'RepositoryName', {
+      value: this.repository.repositoryName,
+    });
+
+    new cdk.CfnOutput(this, 'RepositoryUri', {
+      value: this.repository.repositoryUri,
+    });
+  }
+}
