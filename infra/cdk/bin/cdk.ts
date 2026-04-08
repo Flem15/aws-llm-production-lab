e894eb9fd6a47df9be38bf874500a6b961b2e89c
Elijah <flemingelijah93@gmail.com>
2026-04-07 18:37:28 -0600

    Align CDK synthesizer with bootstrap qualifier prod01

--- infra/cdk/bin/cdk.ts
+++ infra/cdk/bin/cdk.ts
@@ -1,20 +1,21 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import { DefaultStackSynthesizer } from 'aws-cdk-lib';
 import { EcrStack } from '../lib/ecr-stack';
 import { NetworkStack } from '../lib/network-stack';
 
 const app = new cdk.App();
 
-new EcrStack(app, 'LlmParityDevEcrStack', {
+const commonProps: cdk.StackProps = {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: 'us-east-2',
   },
-});
+  synthesizer: new DefaultStackSynthesizer({
+    qualifier: 'prod01',
+  }),
+};
 
-new NetworkStack(app, 'LlmParityDevNetworkStack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: 'us-east-2',
-  },
-});
+new EcrStack(app, 'LlmParityDevEcrStack', commonProps);
+
+new NetworkStack(app, 'LlmParityDevNetworkStack', commonProps);
